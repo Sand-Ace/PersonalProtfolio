f8a6ef23d6e763e7844d7843caffe034a3f773ea
Sand-Ace <sandeshk850@gmail.com>
2024-08-14 19:33:40 +0545

    first commit - project setup and added navbar component

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,75 @@
+import {
+  Bars3BottomRightIcon,
+  EnvelopeIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/solid";
+
+export default function Navbar() {
+  return (
+    <>
+      <header className="header">
+        <h2 className="logo">
+          Sand<span className="logo-span">.dev</span>
+        </h2>
+        <nav className="main-nav">
+          <ul className="nav-item">
+            <li>
+              <a href="#home" className="main-nav-link">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#about me" className="main-nav-link">
+                About me
+              </a>
+            </li>
+            <li>
+              <a href="#projects" className="main-nav-link">
+                My projects
+              </a>
+            </li>
+            <li>
+              <a href="#contact me" className="main-nav-link">
+                Contact me
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className="cta-resumebtn">
+          <button className="btn btn--outline">
+            <EnvelopeIcon width={20} className="envelop-icon" />
+            <span>Resume</span>
+          </button>
+        </div>
+        <div className="hamburger-cta">
+          <Bars3BottomRightIcon width={20} />
+          {/* <XMarkIcon width={20} /> */}
+        </div>
+      </header>
+      <nav className="sub-nav">
+        <ul className="sub-nav-item">
+          <li>
+            <a href="#home" className="sub-nav-link">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about me" className="sub-nav-link">
+              About me
+            </a>
+          </li>
+          <li>
+            <a href="#projects" className="sub-nav-link">
+              My projects
+            </a>
+          </li>
+          <li>
+            <a href="#contact me" className="sub-nav-link">
+              Contact me
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
